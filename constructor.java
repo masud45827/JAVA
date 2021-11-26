b2c431020335c6c2354fbb31341a6d1b97c13119
masud45827 <60808203+masud45827@users.noreply.github.com>
2021-11-26 02:46:45 -0800

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,18 @@
+package javaapplication24;
+class  A{
+     int age;
+      String name,id;
+      A(String name1,String id1,int age1){
+          name=name1; age=age1; id=id1;
+      }
+}
+public class JavaApplication24 {
+    public static void main(String[] args) {
+         A ob=new A("Masud Ali","18CSE027",21);
+         System.out.println("Name = "+ob.name);
+           System.out.println("Name = "+ob.age);
+             System.out.println("Name = "+ob.id);
+         
+    }
+    
+}
