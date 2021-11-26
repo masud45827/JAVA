eef6fc49a29ce5ee4ff9cb7a431d674713052698
masud45827 <60808203+masud45827@users.noreply.github.com>
2021-11-26 02:44:11 -0800

    Create class and object.java

---
+++ class and object.java
@@ -0,0 +1,18 @@
+package javaapplication24;
+class  A{
+     int age;
+      String name,id;
+}
+public class JavaApplication24 {
+    public static void main(String[] args) {
+         A ob=new A();
+         ob.age=21;
+         ob.id="18CSE027";
+         ob.name="Masud Ali";
+         System.out.println("Name = "+ob.name);
+           System.out.println("Name = "+ob.age);
+             System.out.println("Name = "+ob.id);
+         
+    }
+    
+}
